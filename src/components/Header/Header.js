91c9d94b0fdd8f907da2589119d0743d12ae9123
omer <oogur348@gmail.com>
2022-07-01 17:16:08 +0300

    header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import  '../Header/header.css'
 const Header = ({winner}) => {
   return (
     <div>
-              <div className='header'>GAME TİME <span className='won'>{winner} KAZANDI</span></div>
+              <div className='header'>GAME TİME <span className='won'>{winner} WON</span></div>
     </div>
   )
 }
